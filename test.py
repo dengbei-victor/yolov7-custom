095fe8bca64a2d73833903ec46f979d3ee7d6d04
邓贝 <5696332+victorHome@user.noreply.gitee.com>
2022-11-19 13:16:32 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+
+# p = Path()
+# save_path = str(save_dir / p.name)  # img.jpg
+# print(save_path)
+with open("test/output/labels/HT_VAL_000001_SH_001"+'.txt', 'a') as f:
+    f.write("123")
